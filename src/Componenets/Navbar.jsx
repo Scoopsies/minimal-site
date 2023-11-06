91d35da68ae0dae80f5ebde01e29e642461f0e18
Scoopsies <mooninites103@gmail.com>
2023-11-06 10:06:17 -0600

    added projects page and navSelected property to current page

--- src/Componenets/Navbar.jsx
+++ src/Componenets/Navbar.jsx
@@ -1,14 +1,20 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const Navbar = () => {
+  const {pathname} = useLocation();
+  console.log(pathname)
+
   return (
     <nav>
         <ul>
-            <Link to={'/'}>
+            <Link to={'/'} className={pathname === '/' ? 'selectedNav' : null}>
                 <li>Home</li>
             </Link>
-            <Link to={'/contact'}>
+            <Link to={'/contact'} className={pathname === '/contact' ? 'selectedNav' : null}>
                 <li>Contact</li>
+            </Link>
+            <Link to={'/projects'} className={pathname === '/projects' ? 'selectedNav' : null}>
+              <li>Projects</li>
             </Link>
         </ul>
     </nav>
