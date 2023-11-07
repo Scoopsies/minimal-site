8cfd3ccd333ed893786f5cad2857823d43eb0b9f
Scoopsies <mooninites103@gmail.com>
2023-11-07 13:02:44 -0600

    added links to all contact logos

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -9,31 +9,38 @@ const Contact = () => {
         exit={{opacity: 1}}
         className="contact"
       >
-      <div className="nameContainer">
-        <m.h1
-          initial={{ y: "110%" }}
-          animate={{ y: "0%" }}
-          transition={{ delay: 0.5, ease: "easeOut" }}
-          exit={{ opacity: 0 }}
-          className="name"
-        >
-          Contact Info
-        </m.h1>
-      </div>
-          <div className="contactLogos">
+        <div className="nameContainer">
+          <m.h1
+            initial={{ y: "110%" }}
+            animate={{ y: "0%" }}
+            transition={{ delay: 0.5, ease: "easeOut" }}
+            exit={{ opacity: 0 }}
+            className="name"
+          >
+            Contact Info
+          </m.h1>
+        </div>
+        
+        <div className="contactLogos">
 
+          <a href="https://www.linkedin.com/in/consolelog-michael-martin/" target="_blank" rel="noreferrer">
             <div className="logoOverflow">
-             <img className="logo" src="/Images/linkedIn.png" />
+              <img className="logo" src="/Images/linkedIn.png" />
             </div>
-            
+          </a>
+          
+          <a href="https://github.com/scoopsies" target="_blank" rel="noreferrer">
             <div className="logoOverflow">
-             <img className="logo" src="/Images/GitHub.png" />
+              <img className="logo" src="/Images/GitHub.png" />
             </div>
+          </a>
 
+          <a href="mailto:consolelogmichaelmartin@gmail.com">
             <div className="logoOverflow">
-             <img className="logo" src="/Images/Email.png" />
+              <img className="logo" src="/Images/Email.png" />
             </div>
-      </div>
+          </a>
+        </div>
       </m.main>
     )
   }
