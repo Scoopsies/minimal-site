d8c481dd662ceb489e5fe8fd8e23b9ad7163934a
Scoopsies <mooninites103@gmail.com>
2023-11-09 18:13:19 -0600

    Added text on top of bottom image for contact logo

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -30,7 +30,8 @@ const Contact = () => {
 
           <a href="https://www.linkedin.com/in/consolelog-michael-martin/" target="_blank" rel="noreferrer">
             <div className="logoOverflow">
-              <img className="logo logo-back" src="https://media.licdn.com/dms/image/D4D35AQEHHS_kQTIVTg/profile-framedphoto-shrink_200_200/0/1694128022350?e=1700168400&v=beta&t=uzDLwwerQuWWIZRx1kPa4i81rD_R596JkxfeRV1yjZM" />
+                <img className="logo" src="https://media.licdn.com/dms/image/D4D35AQEHHS_kQTIVTg/profile-framedphoto-shrink_200_200/0/1694128022350?e=1700168400&v=beta&t=uzDLwwerQuWWIZRx1kPa4i81rD_R596JkxfeRV1yjZM" />
+                <p className="logoText linkedin">Linkedin</p>
               <img className="logo logo-front" src="/Images/linkedIn.png" />
             </div>
           </a>
@@ -38,6 +39,7 @@ const Contact = () => {
           <a href="https://github.com/scoopsies" target="_blank" rel="noreferrer">
             <div className="logoOverflow">
               <img className="logo" src="https://avatars.githubusercontent.com/u/138405614?v=4" />
+              <p className="logoText github">GitHub</p>
               <img className="logo logo-front" src="/Images/GitHub.png" />
             </div>
           </a>
@@ -45,6 +47,7 @@ const Contact = () => {
           <a href="mailto:consolelogmichaelmartin@gmail.com">
             <div className="logoOverflow">
               <img className="logo" src="https://media3.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif?cid=ecf05e47zzymzsjfb9ze88ddqt0upbdam4iz9sgztlbuactw&ep=v1_gifs_search&rid=giphy.gif&ct=g" />
+              <p className="logoText email">E-Mail</p>
               <img className="logo logo-front" src="/Images/Email.png" />
             </div>
           </a>
