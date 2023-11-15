59eaa9fe9bf6346e40cd093074e0048ca9611b41
Scoopsies <mooninites103@gmail.com>
2023-11-15 12:16:36 -0600

    fixed navbar having background too early for animation

--- src/Componenets/Navbar.jsx
+++ src/Componenets/Navbar.jsx
@@ -10,10 +10,10 @@ const Navbar = () => {
             <Link to={'/'} className={pathname === '/' ? 'selectedNav' : null}>
                 <li>Home</li>
             </Link>
-            <Link to={'/contact'} className={pathname === '/contact' ? 'selectedNav contact' : null}>
+            <Link to={'/contact'} className={pathname === '/contact' ? 'selectedNav' : null}>
                 <li>Contact</li>
             </Link>
-            <Link to={'/projects'} className={pathname === '/projects' ? 'selectedNav projects' : null}>
+            <Link to={'/projects'} className={pathname === '/projects' ? 'selectedNav' : null}>
               <li>Projects</li>
             </Link>
         </ul>
